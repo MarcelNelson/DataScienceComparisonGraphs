40e500c7b8bd45dc78cd9a4ab89a0f3f597880bb
MarcelNelson <marcelnelson2001@gmail.com>
2022-08-08 19:08:53 -0400

    Changes 8/8

--- app/crud.py
+++ app/crud.py
@@ -2,4 +2,4 @@ from sqlalchemy.orm import Session
 from . import models
 
 def get_salary(db:Session):
-    return db.query(models.salary.player, models.salary.Position, models.salary.team).all()
+    return db.query(models.salary.player, models.salary.Position, models.salary.team, models.salary.salary).all()
