24b124b9d913dac610c4cb328752fce8cd5cce73
MarcelNelson <marcelnelson2001@gmail.com>
2022-08-12 12:40:18 -0400

    summary

--- app/app.py
+++ app/app.py
@@ -32,7 +32,7 @@ async def welcome(request: Request, db: Session=Depends(get_db)):
   px.defaults.height = 200
 
 
-  fig = px.bar(df.head(10),x='Player', y='Salary',title='Top 10 paid NFL Players')
+  fig = px.bar(df.head(10),x='Player', y='Salary', color='Position').update_xaxes(categoryorder="total descending")
   fig.update_layout(yaxis=dict(tickfont=dict(size=5)),
   xaxis = dict(tickfont = dict(size=5)),
   font=dict(size=5),
@@ -42,12 +42,49 @@ async def welcome(request: Request, db: Session=Depends(get_db)):
   dfteam=df.groupby('Team')['Salary'].sum()
   dfteam=dfteam.reset_index()
   dfteam=dfteam.sort_values('Salary',ascending=False).head(10)
+  dfteam=df.loc[df['Team'].isin(dfteam.Team)]
 
-  fig10=px.bar(dfteam,x='Team',y='Salary')
-  fig10.update_layout(yaxis=dict(tickfont=dict(size=5)),
+  figteam=px.bar(dfteam,x='Team',y='Salary', color='Position').update_xaxes(categoryorder="total descending")
+  figteam.update_layout(yaxis=dict(tickfont=dict(size=5)),
   xaxis=dict(tickfont=dict(size=5)),
   font=dict(size=5),
   margin=dict(l=0, r=0, t=0, b=0))
-  team10=fig10.to_html(full_html=False, include_plotlyjs='cdn')
+  teamsalary=figteam.to_html(full_html=False, include_plotlyjs='cdn')
 
-  return templates.TemplateResponse("chart.html", {"request": request, "top10": top10, "team10": team10})
+  pos10=dfteam.groupby('Position')['Salary'].mean().sort_values(ascending=False).head(10)
+  pos10=pos10.reset_index()
+  figpos=px.box(dfteam.loc[dfteam['Position'].isin(pos10.Position)], x='Position', y='Salary')
+  figpos.update_layout(yaxis=dict(tickfont=dict(size=5)),
+  xaxis=dict(tickfont=dict(size=5)),
+  font=dict(size=5),
+  margin=dict(l=0, r=0, t=0, b=0))
+  possalary=figpos.to_html(full_html=False, include_plotlyjs='cdn')
+
+  bottom10 = df.groupby('Team')['Salary'].sum()
+  bottom10 = bottom10.reset_index()
+  bottom10 = dfteam.sort_values('Salary', ascending=False).tail(10)
+  dfteam = df.loc[df['Team'].isin(bottom10.Team)]
+
+  pos10 = dfteam.groupby('Position')['Salary'].mean().sort_values(ascending=False).head(10)
+  pos10 = pos10.reset_index()
+  figpos2 = px.box(dfteam.loc[dfteam['Position'].isin(pos10.Position)], x='Position', y='Salary', color_discrete_sequence=['red'])
+  figpos2.update_layout(yaxis=dict(tickfont=dict(size=5)),
+  xaxis = dict(tickfont=dict(size=5)),
+  font=dict(size=5),
+  margin=dict(l=0, r=0, t=0, b=0))
+  possalary2 = figpos2.to_html(full_html=False, include_plotlyjs='cdn')
+
+  dfteam = df.groupby('Position')['Salary'].mean()
+  dfteam = dfteam.reset_index()
+  dfteam.sort_values('Salary', ascending=False)
+
+  figpie = px.pie(dfteam, values='Salary', names='Position')
+  figpie.update_layout(yaxis=dict(tickfont=dict(size=5)),
+  xaxis = dict(tickfont=dict(size=5)),
+  font=dict(size=5),
+  margin=dict(l=0, r=0, t=0, b=0))
+  pospie = figpie.to_html(full_html=False, include_plotlyjs='cdn')
+
+
+
+  return templates.TemplateResponse("chart.html", {"request": request, "top10": top10, "teamsalary": teamsalary, "possalary": possalary, "possalary2": possalary2, "pospie": pospie})
